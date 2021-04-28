89c8f75aaa0ad438d3fc64fda06bb1bcdeab7146
Allan Renard <krahxy.fr@gmail.com>
2021-04-28 12:51:25 +0200

    Ajout du htmlspecialchars_decode sur les echo

--- view/editArticle.php
+++ view/editArticle.php
@@ -13,12 +13,12 @@
 
     <section class="actualityContent">
         <h2>Vous pouvez modifier votre article ci-dessous<br /></h2>
-        <form action="index.php?action=editPanelPost-id-<?php echo $articleOnce['id']?>" method="post">
+        <form action="index.php?action=editPanelPost-id-<?php echo htmlspecialchars_decode($articleOnce['id'])?>" method="post">
             </br></br>
             <div class="page_title">
-                <input type="text" placeholder="Titre" value="<?php echo $articleOnce['title']?>" name="title" class="form-control title" />
+                <input type="text" placeholder="Titre" value="<?php echo htmlspecialchars_decode($articleOnce['title'])?>" name="title" class="form-control title" />
             </div></br></br>
-            <textarea class="mytextarea" placeholder="Racontez-nous une histoire" name="content" cols="50" rows="8"><?php echo $articleOnce['content']?></textarea> </br>
+            <textarea class="mytextarea" placeholder="Racontez-nous une histoire" name="content" cols="50" rows="8"><?php echo htmlspecialchars_decode($articleOnce['content'])?></textarea> </br>
             <input type="submit" class="btn btn-primary" />
         </form>
     </section>
